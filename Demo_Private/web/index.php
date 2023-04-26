8344cdbaaf96fa879ed095a95a88c59a24de25d6
root <root@root.root>
2023-04-26 16:19:47 +0000

    tetetetetetet

---
+++ Demo_Private/web/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Products</title>
+  </head>
+  <body>
+    <table>
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr><td>Teddy Bear</td><td>10</td><td>100</td></tr><tr><td>Car</td><td>20</td><td>200</td></tr><tr><td>Doll</td><td>30</td><td>300</td></tr><tr><td>Train</td><td>40</td><td>400</td></tr><tr><td>Snakes</td><td>50</td><td>500</td></tr>      </tbody>
+    </table>
+  </body>
+</html>
